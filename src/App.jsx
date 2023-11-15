28bca6208946448e4dac994c4f2bb5d0d025e0c6
Onyeabor Joel <earlmorningstar@gmail.com>
2023-11-15 10:56:20 +0100

    UserInput Commit.

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
 import Header from "./components/Header";
+import UserInput from "./components/UserInput";
 
 function App() {
   return (
   <>
   <Header />
+  <UserInput />
 
   </>
     
   )
 }
 
-export default App
+export default App;
--- src/components/UserInput.jsx
+++ src/components/UserInput.jsx
@@ -1,3 +1,60 @@
-export default function UserInput(){
+import { useState } from "react";
 
+export default function UserInput() {
+  const [initInvt, setInitInvt] = useState('');
+  const [annlInvt, setAnnlInvt] = useState('');
+  const [expdrtn, setExptRtn] = useState('');
+  const [duration, setDuration] = useState('');
+
+
+  function handleInitialInvestment(evt){
+    setInitInvt(evt.target.value);
+  }
+
+  function handleAnnualInvestment(evt) {
+    setAnnlInvt(evt.target.value);
+  }
+
+  function handleExpectedReturn(evt){
+    setExptRtn(evt.target.value);
+  }
+
+  function handleDuration(evt){
+    setDuration(evt.target.value);
+  }
+
+
+
+
+
+  return (
+    <>
+      <form className="input-group" id="user-input">
+
+        <div>
+          <span>
+            <label htmlFor="">Initial Investment</label>
+            <input type="number" required value={initInvt} onChange={handleInitialInvestment}/>
+          </span>
+
+          <span>
+             <label htmlFor="">Expected Return</label>
+            <input type="number" required value={expdrtn} onChange={handleExpectedReturn}/>
+          </span>
+        </div>
+
+        <div>
+          <span>
+          <label htmlFor="">Annual Investment</label>
+            <input type="number" required value={annlInvt} onChange={handleAnnualInvestment} />
+          </span>
+
+          <span>
+            <label htmlFor="">Duration</label>
+            <input type="number" required value={duration} onChange={handleDuration} />
+          </span>
+        </div>
+      </form>
+    </>
+  );
 }
